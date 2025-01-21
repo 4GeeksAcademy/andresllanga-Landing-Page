042cc8bb042eb2dc25f0e20fe6825edc1da9e026
Andrés <168719669+andresllanga@users.noreply.github.com>
2025-01-21 17:00:15 +0000

    First Commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="text-center">
+			<footer className="py-5 bg-dark">
+            <div className="container">
+                <p className="m-0 text-center text-white">Copyright © <a href="https://github.com/andresllanga" >AndresLLanga</a></p>
+            </div>
+        </footer>
+		</div>
+	);
+};
+export default Footer;
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Jumbotron = () => {
+return(
+    <div class="container py-5">
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">¡Bienvenido!</h1>
+            <p class="col-md-8 fs-4">Este es un ejemplo de un jumbotron creado con Bootstrap 5. Puedes personalizar el contenido, los colores y el estilo según tus necesidades.</p>
+            <button class="btn btn-primary btn-lg" type="button">Botón de ejemplo</button>
+        </div>
+    </div>
+</div>
+)
+}
+export default Jumbotron;
